a6384fa8bbe21b588a6aa454bfa9da37b72f9474
miriam <miriam_glzr@hotmail.com>
2023-02-01 11:57:17 +0100

    remove eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World! I am Miriam. This is a simple counting app.</p>
+        <p>Count: {count}</p>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
       </header>
     </div>
   );
